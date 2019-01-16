730248d96be4899781e6cc2fd012fb37cb674a3e
Paul Kippes <kippesp@gmail.com>
2019-01-15 22:33:50 -0800

    Add helper script to define a few bash shortcuts

---
+++ myscripts/setenv.sh
@@ -0,0 +1,113 @@
+# setenv.sh - project workflow shortcuts
+
+export OS161=$HOME/projects/ops-class-os161
+export OS161_SRC=$OS161/os161.git
+export PATH=$OS161/tools/bin:$PATH
+export OS161_SYSROOT=$OS161/root
+
+export CURRENT_KERNEL=DUMBVM
+#export CURRENT_KERNEL=ASST1
+#export CURRENT_KERNEL=ASST2
+#export CURRENT_KERNEL=ASST3
+
+# configure gdb for SYS161 connection
+gdb() {
+    pushd $OS161_SYSROOT
+    os161-gdb --eval-command="target remote unix:.sockets/gdb" kernel
+    popd
+}
+
+# go to main source dir
+gosrc() {
+    cd $OS161_SRC
+}
+
+# go to sys/root dir
+gosys() {
+    cd $OS161_SYSROOT
+}
+
+# go to kernel dir
+gokern() {
+    cd $OS161_SRC/kern
+}
+
+# buildkernel
+bk() {
+    pushd $OS161_SRC/kern/compile/$CURRENT_KERNEL
+    bmake
+
+    if [ $? -eq 0 ]; then
+        bmake install
+    fi
+    popd
+}
+
+# build userspace tools
+btools() {
+    pushd $OS161_SRC
+    bmake
+
+    if [ $? -eq 0 ]; then
+        bmake install
+    fi
+    popd
+}
+
+# start sys161 kernel
+st() {
+    pushd $OS161_SYSROOT
+    sys161 kernel
+    popd
+}
+
+# only required to remake defs.mk (to set the root path info)
+os161config() {
+    pushd $OS161_SRC
+    ./configure --ostree=$OS161_SYSROOT
+    popd
+}
+
+kernelconfig() {
+    cd $OS161_SRC/kern/conf
+    ./config $1
+}
+
+# help
+h() {
+    echo "COMMANDS for OS161/SYS161 (kernel: $CURRENT_KERNEL):"
+    echo ""
+    echo "os161config       - initial config for fresh code"
+    echo "kernelconfig CFG  - configure the kernel for CFG"
+    echo "bk                - build kernel"
+    echo ""
+    echo "gosrc  - change to source dir"
+    echo "gosys  - change to OS161_ROOT dir"
+    echo "gokern - change to OS161_SRC/kern dir"
+    echo ""
+    echo "btools - build userspace tools"
+    echo "st     - start sys161 kernel VM"
+    echo ""
+    echo "gdb    - start os161-gdb and connect to SYS161"
+    echo ""
+    echo "h      - show this help"
+}
+
+# Export my functions
+
+export -f os161config
+export -f kernelconfig
+export -f gosrc
+export -f gosys
+export -f gokern
+export -f bk
+export -f btools
+export -f st
+export -f h
+export -f gdb
+
+# config new conf.kern with:
+#   cd ~/projects/ops-class-os161/os161.git/kern/conf
+#   ./config ASST1
+#   ./config ASST2
+#   ./config DUMBVM
