b0abfb35fba7d694ccaf7bea9491ba7c119571df
Paul Kippes <kippesp@gmail.com>
2019-02-12 22:06:10 -0800

    Add PID to tmux session name

--- myscripts/start_tmux.sh
+++ myscripts/start_tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BAR=/home/kippesp/projects/ops-class-os161/setenv.sh
-tmux new-session -s os161 \; \
+tmux new-session -s os161_$$ \; \
   setenv TMUX_SESSION_HOOK $BAR \; \
   send-keys -t 0 "export TMUX_SESSION_HOOK="$BAR C-m \; \
   send-keys -t 0 '. $TMUX_SESSION_HOOK' C-m \; \
