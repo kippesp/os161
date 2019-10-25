31f2418edc0ef57a16df7ddeddfbe0339bc1b8d7
Paul Kippes <kippesp@gmail.com>
2019-10-25 11:04:47 -0700

    Added smoke test for asst2

--- userland/mytests/asst2final/asst2final.c
+++ userland/mytests/asst2final/asst2final.c
@@ -1,14 +1,34 @@
-// Source: https://courses.cs.washington.edu/courses/cse451/14sp/a2.html
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-char  *filename = "/bin/cp";
-char  *args[4];
-pid_t  pid;
+int main(void)
+{
+  pid_t pid = fork();
 
-args[0] = "cp";
-args[1] = "file1";
-args[2] = "file2";
-args[3] = NULL;
+  if (pid == -1) {
+    printf("fork failed\n");
+    exit(1);
+  }
 
-pid = fork();
-if (pid == 0) execv(filename, argv);
+  if (pid == 0) {
+    const char* filename = "/mytests/echo";
+    const char* const args[] = {"echo", "Hello", "World,", "I'm",
+                                "the",  "child", NULL};
 
+    int res = execv(filename, (char* const*)args);
+
+    printf("execv() returned %d--bummer!\n", res);
+  }
+  else {
+    int status;
+    pid_t pid_ret = waitpid(pid, &status, 0);
+    printf("Child returned with %d to parent\n", status);
+    if (pid_ret != pid) {
+      printf("unmatched pid");
+    }
+  }
+
+  return 0;
+}
